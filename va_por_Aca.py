e5307b72c35a2701407b7a57c3b3c3a1a5557e07
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2020-05-06 00:57:27 -0300

    version 6_5 _perfeccionamos la ultima 1AM
    mentira es al de arriba

--- va_por_Aca.py
+++ va_por_Aca.py
@@ -3,9 +3,9 @@ import os
 import cv2
 
 
-KNOWN_FACES_DIR = './conocida/'
+KNOWN_FACES_DIR = './fotos/'
 #UNKNOWN_FACES_DIR = 'frecon/unknown/'
-TOLERANCE = 0.5
+TOLERANCE = 0.4
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
 MODEL = 'hog'  # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
@@ -30,20 +30,21 @@ for name in os.listdir(KNOWN_FACES_DIR):
 
     # Next we load every file of faces of known person
     for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
-
+       
         # Load an image
         image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
 
         # Get 128-dimension face encoding
         # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
-        encoding = face_recognition.face_encodings(image)[0]
+        try:
+            encoding = face_recognition.face_encodings(image)[0]
 
+        except:
+            print("peto: "+str(filename))
         # Append encodings and name
         known_faces.append(encoding)
         known_names.append(name)
 
-
-print('Processing unknown faces...')
 # Now let's loop over a folder of faces we want to label
 while True:
 
@@ -64,6 +65,7 @@ while True:
 
     # But this time we assume that there might be more faces in an image - we can find faces of dirrerent people
     print(f', found {len(encodings)} face(s)')
+
     for face_encoding, face_location in zip(encodings, locations):
 
         # We use compare_faces (but might use face_distance as well)
@@ -74,6 +76,7 @@ while True:
         # then label (name) of first matching known face withing a tolerance
         match = None
         if True in results:  # If at least one is true, get a name of first of found labels
+            
             match = known_names[results.index(True)]
             print(f' - {match} from {results}')
 
@@ -97,6 +100,31 @@ while True:
 
             # Wite a name
             cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+
+        if match == None:
+            match = 'unknown'
+             # Each location contains positions in order: top, right, bottom, left
+            top_left = (face_location[3], face_location[0])
+            bottom_right = (face_location[1], face_location[2])
+
+            # Get color by name using our fancy function
+            color = name_to_color(match)
+
+            # Paint frame
+            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+            # Now we need smaller, filled grame below for a name
+            # This time we use bottom in both corners - to start from bottom and move 50 pixels down
+            top_left = (face_location[3], face_location[2])
+            bottom_right = (face_location[1], face_location[2] + 22)
+
+            # Paint frame
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+
+            # Wite a name
+            cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+
+
         # else:
         #     match = 'unknown'
         #      # Each location contains positions in order: top, right, bottom, left
