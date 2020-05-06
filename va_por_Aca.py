cb9f01a80bfa727db4b9017df6372dda73a49468
Fabro0 <47898082+Fabro0@users.noreply.github.com>
2020-05-05 22:59:39 -0300

    version 5/5 renovada
    al de abajo lo violan

--- va_por_Aca.py
+++ va_por_Aca.py
@@ -3,12 +3,14 @@ import os
 import cv2
 
 
-KNOWN_FACES_DIR = 'frecon/conocida/'
-UNKNOWN_FACES_DIR = 'frecon/unknown/'
-TOLERANCE = 0.6
+KNOWN_FACES_DIR = './conocida/'
+#UNKNOWN_FACES_DIR = 'frecon/unknown/'
+TOLERANCE = 0.5
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
 MODEL = 'hog'  # default: 'hog', other one can be 'cnn' - CUDA accelerated (if available) deep-learning pretrained model
+
+video = cv2.VideoCapture(2) #could put in a filename
 
 # Returns (R, G, B) from name
 def name_to_color(name):
@@ -43,12 +45,12 @@ for name in os.listdir(KNOWN_FACES_DIR):
 
 print('Processing unknown faces...')
 # Now let's loop over a folder of faces we want to label
-for filename in os.listdir(UNKNOWN_FACES_DIR):
+while True:
 
     # Load image
-    print(f'Filename {filename}', end='')
-    image = face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}')
-
+    #print(f'Filename {filename}', end='')
+    #image = face_recognition.load_image_file(f'{UNKNOWN_FACES_DIR}/{filename}')
+    ret,image = video.read() 
     # This time we first grab face locations - we'll need them to draw boxes
     locations = face_recognition.face_locations(image, model=MODEL)
 
@@ -58,7 +60,7 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
 
     # We passed our image through face_locations and face_encodings, so we can modify it
     # First we need to convert it from RGB to BGR as we are going to work with cv2
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    #image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     # But this time we assume that there might be more faces in an image - we can find faces of dirrerent people
     print(f', found {len(encodings)} face(s)')
@@ -95,8 +97,32 @@ for filename in os.listdir(UNKNOWN_FACES_DIR):
 
             # Wite a name
             cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
+        # else:
+        #     match = 'unknown'
+        #      # Each location contains positions in order: top, right, bottom, left
+        #     top_left = (face_location[3], face_location[0])
+        #     bottom_right = (face_location[1], face_location[2])
+
+        #     # Get color by name using our fancy function
+        #     color = name_to_color(match)
+
+        #     # Paint frame
+        #     cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+        #     # Now we need smaller, filled grame below for a name
+        #     # This time we use bottom in both corners - to start from bottom and move 50 pixels down
+        #     top_left = (face_location[3], face_location[2])
+        #     bottom_right = (face_location[1], face_location[2] + 22)
+
+        #     # Paint frame
+        #     cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+
+        #     # Wite a name
+        #     cv2.putText(image, match, (face_location[3] + 10, face_location[2] + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), FONT_THICKNESS)
 
     # Show image
     cv2.imshow(filename, image)
-    cv2.waitKey(0)
-    cv2.destroyWindow(filename)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+    #cv2.waitKey(0)
+    #cv2.destroyWindow(filename)
