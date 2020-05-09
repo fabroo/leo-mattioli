fa628c7fe2b3b7f79bb4c3d36c8eeacd593187dd
Hollow <tievolib@gmail.com>
2020-05-09 20:38:34 -0300

    Y TODA MI GENTE SE MUEVE

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request, render_template, send_from_directory, flash
+from flask import Flask, request, render_template, send_from_directory, flash, g, session, redirect, url_for
 
 __author__ = 'ibininja'
 
@@ -29,82 +29,115 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+class User:
+    def __init__(self, id, fullName, contra, mail):
+        self.id = id
+        self.fullname = fullName
+        self.contra = contra
+        self.mail = mail
+
+comm = sqlite3.connect
+BASE_DIR = os.getcwd()
+db_path = os.path.join(BASE_DIR, "testdb.db")
+conn  = sqlite3.connect(db_path)
+c = conn.cursor()
+
+c.execute(f'''SELECT * FROM residents''')
+pakesepa = c.fetchall()
+userList = []
+for el in pakesepa:
+    userList.append(User(id=str(el[0]),fullName = str(el[1]),contra=str(el[2]),mail=str(el[3])))
 
 @app.route("/")
 def index():
     return render_template("home.html")
 
+@app.before_request
+def before_request():
+    g.user = None
+    
+    if 'user_id' in session:
+        user = [x for x in userList if x.id == session['user_id']][0]
+        g.user = user
+  
+
 
 @app.route("/upload", methods=["GET","POST"])
 def upload():
-    return render_template('upload.html')
-    text = request.form['text']
+    if not g.user:
+        return render_template('login.html')
+    else:
+        return render_template('upload.html')
+        text = request.form['text']
 
-    KNOWN_NAMES = f'{os.getcwd()}/fotos/'
-    existe = False
+        KNOWN_NAMES = f'{os.getcwd()}/fotos/'
+        existe = False
 
-    target = os.path.join(APP_ROOT,'images/'+str(text)+'')
-    print(target)
-    
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    print(request.files.getlist("file"))
+        target = os.path.join(APP_ROOT,'images/'+str(text)+'')
+        print(target)
+        
+        if not os.path.isdir(target):
+            os.mkdir(target)
+        print(request.files.getlist("file"))
 
 
-    for upload in request.files.getlist("file"):
-        print(upload)
-        print("{} is the file name".format(upload.filename))
-        filename = upload.filename
-        # This is to verify files are supported
-        ext = os.path.splitext(filename)[1]
-        if (ext == ".jpg") or (ext == ".png"):
-            print("File supported moving on...")
-        else:
-            render_template("Error.html", message="Files uploaded are not supported...")
-        destination = "/".join([target, filename])
-        print("Accept incoming file:", filename)
-        print("Save it to:", destination)
-        upload.save(destination)
+        for upload in request.files.getlist("file"):
+            print(upload)
+            print("{} is the file name".format(upload.filename))
+            filename = upload.filename
+            # This is to verify files are supported
+            ext = os.path.splitext(filename)[1]
+            if (ext == ".jpg") or (ext == ".png"):
+                print("File supported moving on...")
+            else:
+                render_template("Error.html", message="Files uploaded are not supported...")
+            destination = "/".join([target, filename])
+            print("Accept incoming file:", filename)
+            print("Save it to:", destination)
+            upload.save(destination)
 
-    for folder_name in KNOWN_NAMES:
-        if text == folder_name:
-            existe = True
-    
-    try:
-        if existe:
-            print('Ya existe un directorio con tu nombre, agregando nuevas fotos...')
-            addExtraPics.nuevasFotos(text) #usa el autocomplete, y esta funcion toma name como argumento
-            flash('Ya se ejecutó y todo salio bien segui viviendo')
-        else:
-            print('Nuevo directorio, entrenando reconocimiento facial...')
-            trainPeroBien.trainearBien()#creo que era??
-            flash('Ya se ejecutó y todo salio bien segui viviendo')
-    except:
-         flash('Amigo se cayo todo sorry cambia de empresa..')
-    
+        for folder_name in KNOWN_NAMES:
+            if text == folder_name:
+                existe = True
+        
+        try:
+            if existe:
+                print('Ya existe un directorio con tu nombre, agregando nuevas fotos...')
+                addExtraPics.nuevasFotos(text) #usa el autocomplete, y esta funcion toma name como argumento
+                flash('Ya se ejecutó y todo salio bien segui viviendo')
+            else:
+                print('Nuevo directorio, entrenando reconocimiento facial...')
+                trainPeroBien.trainearBien()#creo que era??
+                flash('Ya se ejecutó y todo salio bien segui viviendo')
+        except:
+            flash('Amigo se cayo todo sorry cambia de empresa..')
     
     return render_template("upload.html", image_name=filename)
 
+@app.route('/close_sess', methods=['GET', 'POST'])
+def closeSess():
+    session.pop('user_id', None)
+    return render_template('login.html')
 
-@app.route("/login",methods = ["GET","POST"])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # return render_template("login.html")
-    try:
-        user = request.form['user']
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['user']
         password = request.form['password']
-        print(user)
-        print(password)
-        if inputId.inputear(password,user):
-            flash('Bienvenido, '+user+' que te contas?')
+        
+
+        if inputId.inputear(password,username):
+            user = [x for x in userList if x.fullname == username][0]
+            session['user_id'] = user.id
             return render_template('home.html')
-             
 
         else:
             flash('not a valid username!, try again','warning')
-        return render_template('login.html')
-    except Exception as err:
-        print('ESTE ES EL ERROR REY E' +str(err))
-        return render_template('login.html')
+    
+    return render_template('login.html')
 
 @app.route("/register",methods = ["GET","POST"])
 def register():
